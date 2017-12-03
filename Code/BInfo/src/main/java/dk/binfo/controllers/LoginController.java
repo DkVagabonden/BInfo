94174678974dba96cc91a5f5fcf8f50a1df25a6d
MortenGert <mortengertolsen@gmail.com>
2017-12-03 16:44:17 +0100

    Revert "Revert "Revert "Controllers divided"""
    This reverts commit 83942afd7e5c88acfab4adac0a0be7cfc5bae40d.

--- Code/BInfo/src/main/java/dk/binfo/controllers/LoginController.java
+++ Code/BInfo/src/main/java/dk/binfo/controllers/LoginController.java
@@ -1,14 +1,20 @@
 package dk.binfo.controllers;
 
-import dk.binfo.models.User;
-import dk.binfo.services.UserService;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import dk.binfo.models.User;
+import dk.binfo.services.UserService;
 
 @Controller
 public class LoginController {
@@ -31,10 +37,38 @@ public class LoginController {
 		return modelAndView;
 	}
 
+
 	@RequestMapping(value = {"/accessDenied"}, method = RequestMethod.GET)
 	public ModelAndView accessDenied() {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("/accessDenied");
+		return modelAndView;
+	}
+
+	
+	@RequestMapping(value={"/registration", "/register"}, method = RequestMethod.GET)
+	public ModelAndView registration(){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("user",  new User());
+		modelAndView.setViewName("registration");
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/registration", method = RequestMethod.POST)
+	public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+		ModelAndView modelAndView = new ModelAndView();
+		User userExists = userService.findUserByEmail(user.getEmail());
+		if (userExists != null) {
+			bindingResult.rejectValue("email", "error.user", "Der eksisterer allerede en bruger med den angivne email");
+		}
+		if (bindingResult.hasErrors()) {
+			modelAndView.setViewName("registration");
+		} else {
+			userService.saveUser(user);
+			modelAndView.addObject("successMessage", "SUCCES!: Du har tilføjet en ny bruger.");
+			modelAndView.addObject("user", new User());
+			modelAndView.setViewName("registration");
+		}
 		return modelAndView;
 	}
 
@@ -48,7 +82,77 @@ public class LoginController {
 		modelAndView.setViewName("/home");
 		return modelAndView;
 	}
+	
+	@RequestMapping(value={"/settings/{email:.+}"}, method = RequestMethod.GET)
+	public ModelAndView settings(@PathVariable String email){
+		ModelAndView modelAndView = new ModelAndView();
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		User user = userService.findUserByEmail(auth.getName());
+		modelAndView.addObject("user", user);
+		modelAndView.addObject("userForm",user);
+		modelAndView.setViewName("/settings");
+		return modelAndView;
+	}
+
+	@RequestMapping(value="/settings/{email:.+}", method=RequestMethod.POST)
+	public ModelAndView editPhoneSettings(@ModelAttribute @Valid User userinfo, BindingResult bindingResult, @PathVariable String email){
+		ModelAndView modelAndView = new ModelAndView();
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		User user = userService.findUserByEmail(auth.getName());
+		modelAndView.addObject("user", user);
+
+		if (bindingResult.hasErrors())
+		{
+			modelAndView.setViewName("/settings/{email}");
+		}
+		modelAndView.setViewName("redirect:/settings/{email}");
+		userService.updateUserSettings(userinfo);
+		return modelAndView;
+	}
+
+	@RequestMapping("/users/add")
+	public ModelAndView adminCreateNewUser(){
+		ModelAndView modelAndView = new ModelAndView();
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		User user = userService.findUserByEmail(auth.getName());
+		modelAndView.addObject("user", user);
+		modelAndView.addObject("users", new User());
+		modelAndView.setViewName("/users/add");
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/users/add", method = RequestMethod.POST)
+	public ModelAndView adminCreateUser(@Valid User users, BindingResult bindingResult) {
+		ModelAndView modelAndView = new ModelAndView();
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		User user = userService.findUserByEmail(auth.getName());
+		modelAndView.addObject("user", user);
+		User userExists = userService.findUserByEmail(users.getEmail());
+		if (userExists != null) {
+			bindingResult.rejectValue("email", "error.user", "Der eksisterer allerede en bruger med den angivne email");
+		}
+		else {
+			userService.adminSaveUser(users);
+			modelAndView.addObject("successMessage", "SUCCES!: Du har tilføjet en ny bruger.");
+			modelAndView.addObject("users", new User());
+			modelAndView.setViewName("/users/add");
+		}
+		return modelAndView;
+	}
+
+
+	@RequestMapping("/users")
+	public ModelAndView showUsers() {
+		ModelAndView modelAndView = new ModelAndView("/users", "users", userService.findAll());
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		User user = userService.findUserByEmail(auth.getName());
+		modelAndView.addObject("user", user);
+		modelAndView.setViewName("/users");
+		return modelAndView;
+	}
 
 	//TODO fuldføre submit
+
+
 	
 }
