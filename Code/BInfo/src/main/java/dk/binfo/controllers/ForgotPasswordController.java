b62bd9d985d3f18b19f5e8f81812c3f5d6df8a0b
DjStonie <valdonay@yahoo.dk>
2017-12-07 12:21:36 +0100

    whatever

--- Code/BInfo/src/main/java/dk/binfo/controllers/ForgotPasswordController.java
+++ Code/BInfo/src/main/java/dk/binfo/controllers/ForgotPasswordController.java
@@ -1,19 +1,14 @@
 package dk.binfo.controllers;
 
-import dk.binfo.models.Apartment;
 import dk.binfo.models.User;
+import dk.binfo.services.EmailService;
 import dk.binfo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.validation.Valid;
 
 @Controller
 public class ForgotPasswordController {
@@ -21,10 +16,20 @@ public class ForgotPasswordController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private EmailService emailService;
+
     @RequestMapping(value={"/forgotpassword"}, method = RequestMethod.GET)
     public ModelAndView glemtpassword(){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("forgotpassword");
+        return modelAndView;
+    }
+
+    @RequestMapping(value={"/forgotpasswordlink"}, method = RequestMethod.GET)
+    public ModelAndView glemtpasswordlink(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("forgotpasswordlink");
         return modelAndView;
     }
 
@@ -36,8 +41,26 @@ public class ForgotPasswordController {
             modelAndView.addObject("Message", "Emailen existere ikke, prøv igen.");
         }else{
             modelAndView.addObject("Message", "SUCCES!: Der er sent et link til din email!");
+            sendResetEmail(user);
         }
         return modelAndView;
     }
+
+    public void sendResetEmail(User user){
+        String body = "<B>Reset dit password</B><br><br>";
+        body += "Hvis du har bedt om at få dit password resat,<br> beder vi dig ";
+        body += "beder vi dig klikke på nedenstående link.<br><br>";
+        body += "<a href=\"http://localhost:8080/forgotpassword?user="+user.getPassword()+"\">Klik her for at reset dit password</a><br><br>";
+        body += "Har du ikke bedt om et reset skal du bare ignore denne email.<br><br>";
+        body += "Mvh<br>";
+        body += "Jens Boligforening<br>";
+        body += "nørrebrogade 123<br>";
+        body += "2312 nørrebro";
+
+        emailService.generateAndSendEmail("jensbackvall@t-nova.org","BoligInfo password reset",body);
+    }
+
+
+
 
 }
